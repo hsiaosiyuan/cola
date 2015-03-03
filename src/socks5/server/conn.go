72bf2a9eb1af0218da5b252ebefd6a7543613a4d
mconintet <mconintet@163.com>
2015-03-03 20:43:51 +0800

    remove timeout

--- src/socks5/server/conn.go
+++ src/socks5/server/conn.go
@@ -8,7 +8,6 @@ import (
 	"net"
 	"strconv"
 	"sync"
-	"time"
 )
 
 const (
@@ -287,7 +286,6 @@ func (c *conn) parseDstAddr() error {
 func (c *conn) prepareExchange() error {
 	var (
 		err error
-		t time.Time
 	)
 
 	if c.dstConn, err = net.DialTCP("tcp", nil, c.dstAddr); err != nil {
@@ -313,9 +311,6 @@ func (c *conn) prepareExchange() error {
 			return ErrPrepareExchangeGeneral
 		}
 	}
-
-	t = time.Now().Add(time.Minute*2)
-	c.dstConn.SetDeadline(t)
 
 	return nil
 }
